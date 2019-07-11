849305e3fd563c546170a46005db60044a1353be
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-07-11 20:14:00 -0300

    Cartão aletórios na main page

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from programas.models import Programa
 # Create your views here.
 
 def main_page(request):
     template_name  = 'main_page.html'
-    context = {}
+    context = {
+        "Programas": Programa.objects.order_by('?')[:4]
+    }
     return render(request, template_name, context)
