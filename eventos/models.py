8e355d72064b03a7e4af7f3102b4aa795da20819
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-07-22 14:32:43 -0300

    Criada Models Eventos

--- eventos/models.py
+++ eventos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Evento(models.Model):
+
+    # Inforamções Principais
+    title = models.CharField('Títutlo', max_length=60)
+    desc = models.TextField('Descrição')
+    #  Datas
+    day = models.DateField('Dia')
+    isAllDay = models.BooleanField('É o dia inteiro?')
+    start = models.TimeField('Horário de começo')
+    end = models.TimeField('Horário de término')
+    #  Link para mais informações
+    link = models.URLField('link', blank=True)
