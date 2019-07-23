9547cf259c74d5e086d31f740d3aec314322e48e
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-07-23 17:59:04 -0300

    Adicionando no calendário eventos do bd

--- eventos/models.py
+++ eventos/models.py
@@ -12,5 +12,13 @@ class Evento(models.Model):
     isAllDay = models.BooleanField('É o dia inteiro?')
     start = models.TimeField('Horário de começo')
     end = models.TimeField('Horário de término')
+
     #  Link para mais informações
     link = models.URLField('link', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Eventos'
+        verbose_name = 'Evento'
--- eventos/views.py
+++ eventos/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Evento
+
 
 # Create your views here.
 
 def index(request):
     template_name = 'eventos_index.html'
-    context = {}
+    context = {
+        "Eventos": Evento.objects.all(),
+    }
     return render(request, template_name, context)
