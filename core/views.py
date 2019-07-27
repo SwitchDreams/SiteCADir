788a04928fd2482da8386cbbf173605007be885a
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-07-27 12:03:08 -0300

    Eventos no slider da main_page

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from programas.models import Programa
+from eventos.models import Evento
 # Create your views here.
 
 def main_page(request):
     template_name  = 'main_page.html'
     context = {
-        "Programas": Programa.objects.order_by('?')[:4]
+        "Programas": Programa.objects.order_by('?')[:4],
+        "Eventos": Evento.objects.order_by('start_day')[:3]
     }
     return render(request, template_name, context)
