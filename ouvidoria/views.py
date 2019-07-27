c1f30027ef23b1118b61950e6b646628216bd022
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-07-27 01:21:22 -0300

    Tentando arrumar fullcalendar no site

--- ouvidoria/views.py
+++ ouvidoria/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-from .forms import FormsOuvidoria
-from django.core.mail import send_mail
-from .models import MensagemOuvidoria
 import datetime
+from email.mime.image import MIMEImage
+
+import bs4
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.shortcuts import render
+from django.template.loader import render_to_string
+
+from .forms import FormsOuvidoria
+from .models import MensagemOuvidoria
 
 
 # Auxiliar functions
@@ -25,6 +31,9 @@ def corpo_do_email(assunto, texto, email):
     corpo_do_texto += texto
     return corpo_do_texto
 
+def image_finder(tag):
+    return (tag.name == u'img' or
+            tag.name == u'table' and tag.has_key('background'))
 
 # Create your views here.
 def ouvidoria(request):
@@ -54,9 +63,47 @@ def ouvidoria(request):
         # Email do cadir(Tem que ser uma lista)
         to_email = ["dreamsswitch@gmail.com"]
 
+        html_content = render_to_string('template_email.html', {'conteudo': corpo_do_texto})
+
+        # Processando as imagens do e-mail
+        soup = bs4.BeautifulSoup(html_content)
+        images = []
+        added_images = []
+        for index, tag in enumerate(soup.findAll(image_finder)):
+            if tag.name == u'img':
+                name = 'src'
+            elif tag.name == u'table':
+                name = 'background'
+            # If the image was already added, skip it.
+            if tag[name] in added_images:
+                continue
+            added_images.append(tag[name])
+            images.append((tag[name], 'img%d' % index))
+            tag[name] = 'cid:img%d' % index
+        html_content = str(soup)
+
+        # Testando novo formato de envio
+        email = EmailMultiAlternatives(
+            subject=assunto_enviado,
+            body=html_content,
+            from_email="brad@sandboxd5f011b5c7d4421bb16957131f60fe01.mailgun.org",
+            to=to_email
+        )
+
+        # Adicionando as img no e-mail
+        for filename, file_id in images:
+            image_file = open(settings.BASE_DIR + filename, 'rb')
+            msg_image = MIMEImage(image_file.read())
+            image_file.close()
+            msg_image.add_header('Content-ID', '<%s>' % file_id)
+            email.attach(msg_image)
+
+        email.content_subtype = "html"
+
         # Enviando o e-mail
-        send_mail(assunto_enviado, corpo_do_texto, "brad@sandboxd5f011b5c7d4421bb16957131f60fe01.mailgun.org", to_email, html_message=corpo_do_texto)
+        email.send()
         context["thanks"] = "E-mail enviado com sucesso"
+
     context["form"] = form
 
     return render(request, template_name, context)
