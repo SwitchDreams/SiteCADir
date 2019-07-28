489b0300087929085df3410a778727f54f9a6d14
Joao Pedro Assis <joaopedroassisdossantos@gmail.com>
2019-07-28 12:53:26 -0300

    Models dos eventos

--- guias/admin.py
+++ guias/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Guia
 
 # Register your models here.
+admin.site.register(Guia)
--- guias/models.py
+++ guias/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Guia(models.Model):
+
+    # Link para guia externo
+    link = models.URLField('Link')
+
+    class Meta:
+        verbose_name_plural = "Guias"
+        verbose_name = "Guia"
