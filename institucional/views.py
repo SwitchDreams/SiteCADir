0f19042cd51e748557477106e4ce968f0e53cc11
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-07-29 14:05:56 -0300

    Mudanças no formato das datas

--- institucional/views.py
+++ institucional/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from .models import Postagem
+from datetime import datetime
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
     template_name = 'institucional_index.html'
     context = {
-        "Postagens" : Postagem.objects.order_by('-created_at')
+        "Postagens" : Postagem.objects.order_by('-created_at'),
+        "Hoje": timezone.now()
     }
     return render(request, template_name, context)
 
 def postagem_show(request, **kwargs):
     template_name = 'postagem_show.html'
     context = {
-        "Postagem": Postagem.objects.filter(nome=kwargs['pk']).get()
+        "Postagem": Postagem.objects.filter(nome=kwargs['pk']).get(),
+        "Hoje": timezone.now()
     }
     return render(request, template_name, context)
