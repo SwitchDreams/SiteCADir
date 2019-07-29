013deab6e76285c8b128e31f193d2c8871eeab0c
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-07-29 17:38:55 -0300

    Pagination postagens

--- institucional/views.py
+++ institucional/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Postagem
 from datetime import datetime
 from django.utils import timezone
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -17,8 +18,13 @@ def index(request):
 
 def postagens_index(request):
     template_name = 'postagens_index.html'
+    postagem_list = Postagem.objects.order_by('-created_at')
+    paginator = Paginator(postagem_list, 10)
+
+    page = request.GET.get('page')
+    postagens = paginator.get_page(page)
     context = {
-        "Postagens": Postagem.objects.order_by('-created_at'),
+        "Postagens": postagens,
         "Hoje": timezone.now()
     }
     return render(request, template_name, context)
