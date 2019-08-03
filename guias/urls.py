e279652175e2045db735546d625672c5998e7f24
Joao Pedro Assis <joaopedroassisdossantos@gmail.com>
2019-08-03 17:35:12 -0300

    começando a colocar as rotas

--- guias/urls.py
+++ guias/urls.py
@@ -6,4 +6,7 @@ app_name = 'guias'
 urlpatterns = [
     # Index Path
     path('', views.index, name='guias_index'),
+    path('assistencia_estudantil', views.assistencia_estudantil, name='assistencia_estudantil'),
+    path('aluno_estrangeiro', views.estrangeiro, name='aluno_estrangeiro'),
+    path('fluxo', views.fluxo, name='fluxo'),
 ]
--- guias/views.py
+++ guias/views.py
@@ -8,3 +8,18 @@ def index(request):
         "Guias": Guia.objects.order_by("nome")
     }
     return render(request, template_name, context)
+
+def assistencia_estudantil(request):
+    template_name = 'assistencia_estudantil.html'
+    context = {}
+    return render(request, template_name, context)
+
+def estrangeiro(request):
+    template_name = 'estrangeiro.html'
+    context = {}
+    return render(request, template_name, context)
+
+def fluxo(request):
+    template_name = 'fluxo.html'
+    context = {}
+    return render(request, template_name, context)
