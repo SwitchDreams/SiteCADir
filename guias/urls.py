fed2b8cf82da39c1ff10b076ecc3b22dbff0a645
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-08-10 15:54:10 -0300

    Show dos guias

--- guias/urls.py
+++ guias/urls.py
@@ -6,6 +6,8 @@ app_name = 'guias'
 urlpatterns = [
     # Index Path
     path('', views.index, name='guias_index'),
+    # Show Path
+    path('<int:pk>', views.show, name='guias_show'),
     path('assistencia_estudantil', views.assistencia_estudantil, name='assistencia_estudantil'),
     path('aluno_estrangeiro', views.estrangeiro, name='aluno_estrangeiro'),
     path('fluxo', views.fluxo, name='fluxo'),
