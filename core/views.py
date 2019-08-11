7995f242fcee1274d0ef9a688c526255332f81dc
Joao Pedro Assis <joaopedroassisdossantos@gmail.com>
2019-08-11 12:35:01 -0300

    changes and cleanup

--- core/views.py
+++ core/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from programas.models import Programa
 from eventos.models import Evento
+from guias.models import Guia
 # Create your views here.
 
 def main_page(request):
     template_name  = 'main_page.html'
     context = {
         "Programas": Programa.objects.order_by('?')[:4],
-        "Eventos": Evento.objects.order_by('start_day')[:3]
+        "Eventos": Evento.objects.order_by('start_day')[:3],
+        "Guias": Guia.objects.order_by('?')[:4]
     }
     return render(request, template_name, context)
