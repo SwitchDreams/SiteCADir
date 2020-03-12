c1575c9e842d0dada3057614fb8badb83afefad9
Pedro Augusto Ramalho Duarte <43936240+PedroAugustoRamalhoDuarte@users.noreply.github.com>
2020-03-12 15:56:35 -0300

    static root

--- SiteCaDir/settings.py
+++ SiteCaDir/settings.py
@@ -131,13 +131,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
+STATIC_ROOT      =  os.path.join(BASE_DIR, "static") 
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # E-mail Configuration
 EMAIL_HOST = 'smtp.mailgun.org'
