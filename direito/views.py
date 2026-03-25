69b53f71e3f16407270dd7720e336a2eaf485dac
Kanbino <226095518+kanbino-agent@users.noreply.github.com>
2026-03-25 14:28:52 -0300

    Modificado conforme feedback: renomeado template, expandido parágrafo histórico e adicionado Código Tributário Nacional

--- direito/views.py
+++ direito/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def direito_brasileiro(request):
-    template_name = 'direito_brasileiro.html'
+    template_name = 'direito/historico_direito_brasileiro.html'
     context = {}
     return render(request, template_name, context)
